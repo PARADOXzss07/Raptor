cb132c6e47cdae1ecf71ccad3506ba560538090d
Arco <PARADOXzss07@gmail.com>
2024-09-07 21:51:25 -0400

    Added outline with basic code
    No real python yet

---
+++ main.py
@@ -0,0 +1,64 @@
+## Project R@ptor 
+## First created: 9/6/24
+
+## Imports
+import sys
+import os
+# work on importing base16 color schemes
+
+# - Login screen -
+print("Welcome to")
+print("   /\   /¯¯¯¯\                  /\ ‚                      /\    /¯¯¯\‘         /\                              /¯¯¯\ '            /\   /¯¯¯¯\       ")
+print(" /    \|   |:'\    \              /    \   ‚                /    \/   |\    \°     /    \__________'        /         \‚         /    \|   |:'\    \     ")
+print("|\         |:::|    |           /      /|                  |\         |::'\    \   |                        \°    /            '\°      |\         |:::|    |    ")
+print("|:|        |::/    /|          |      |:'| /¯¯¯\‚         |:|         \:::|    |  |\____         _       \'  |        |\      \°    |:|        |::/    /|    ")
+print("|:|        |/    /::|          |\      \/ /|      |'        |/           '\/    /|  |:|::::::|\       \:|\     /|° |        |::\      \ '‚ |:|        |/    /::|    ")
+print(" \|            (:::'|          |::\___/:/'     /|'       /       /|\____/::'|  |:|::::::|::\       \::\/:::|  |\       '\:::\      \ ' \|            (:::'|    ")
+print("  |      |\      \:/'         /¯¯¯¯¯\/     /::|°    /       /::|:|:::::|::::|   \|::::::|::::\       \:|:::|  |::\       '\::|      |   |      |\      \:/'    ")
+print("  |      |::\      \'       /      /|        /::::|°  /       /::::|:|:::::|::/‘      ¯¯¯ \:::::\       \:::/‘ |::::\       '\|      |   |      |::\      \'    ")
+print("  |      |::::\      \'   /      /:/       /:::::/‘  |       |::::/ \|:::::|/                 \::::|        |/'    \:::::\            /|   |      |::::\      \'  ")
+print(" /'     /|\::::|      '|‘ |'      |/        |:::::/    |\       \:/    ¯¯¯                     \'/        /|‘       \:::::\____ /::|  /'     /|\::::|      '|‘ ")
+print("|\    /::|  \/       /|‘ |\_____/|\     \::/'     |::\      /|'                              |\       /::|'         \::::|::::::|:::'| |\    /::|  \/       /|‘ ")
+print("|::\/::::|   |\    /::|  |:|:::::::|:|::\    /|       |::::\  /::|'                              |::\   /::::|'           \::|::::::|::/‘ '|::\/::::|   |\    /::|  ")
+print("|:::|::::/   |::\/::::|  |:|:::::::|:|::::\/::|        \::::|::::|          '                    |:::'\/:::::/‘             \|::::::|/‘   '|:::|::::/   |::\/::::|  ")
+print("\::'|::/     |:::|::::/° '\|:::::::|/ \::::|::'|‘         \::|:::/     '‚                         \::::|::::/ '‚               ¯¯¯'      \::'|::/     |:::|::::/° ")
+print("  '\|/        \::|::/      ¯¯¯¯     \::|::/            \|:/                  '‚                \::|::/ '‚                 ‘             '\|/        \::|::/    ")
+print("               \|/°                     \|/                                                       \|/°                               ‘               \|/°     ")
+
+## Goals
+# 1. Alternative for Youtube, Facebook, Twitter, Reddit, snapchat, etc
+# 2. Zones for everyone, I.e. drawing/art zone, code zone, Linux zone, etc
+# 3. Safe platform for all ages (Except kids under 10, they don't need
+# to be on social media.)
+# 4. 90% OR more in Python (Not counting databases and the front-end)
+# 5. Respect user privacy
+# 6. Be under the GPL 3.0 license or MIT.
+
+## - USERNAME -
+# Username is stored with the hashed password in a (USERNAMEhere).txt file,
+# then encrypted with AES-256
+
+## - PASSWORD -
+# Password gets hashed locally, then sent to a database
+
+## - FYP -
+
+## Search bar & searching stuff
+
+## - Settings -
+
+# Content filtering options
+# 1. Coding include/exclude
+# 2. Age based content
+# 3. Specific topic include/exclude
+# 4. Curse word blurring/muting
+
+#  - Theming -
+# Base16 themes
+# Light and dark mode
+
+# - advertising settings -
+# 1. Ad include/exclude
+# 2. Ad time limit
+# 3. Ad topic filtering
+
